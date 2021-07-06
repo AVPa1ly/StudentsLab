4fb1d39bc44b4ee83bf1294de4f17d404c7d4e03
AVPa1ly <andrewvorobiov@gmail.com>
2021-07-06 15:50:07 +0300

    Implemented Adapter pattern

---
+++ DesignPatterns/Adapter/IXmlToJson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Adapter
+{
+    interface IXmlToJson
+    {
+        public List<DateTime> ListBookToDateTime(List<Book> books);
+    }
+}
---
+++ DesignPatterns/Adapter/Library.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Adapter
+{
+    class Library
+    {
+        public List<Book> Books { get; private set; }
+
+        public Library(List<Book> books)
+        {
+            Books = books;
+        }
+
+        public List<Book> GetBooks()
+        {
+            return Books;
+        }
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+    }
+}
